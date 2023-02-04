c448665ce31c6c3a61f7c7d3773a028d69be9b5b
Diego H O R Antunes <74326564+D-H-O-R-A@users.noreply.github.com>
2023-02-04 16:36:33 -0300

    Update erc721.sol

--- nft marketplace/erc721.sol
+++ nft marketplace/erc721.sol
@@ -1,36 +1,75 @@
 pragma solidity ^0.8.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/token/ERC721/ERC721.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/access/Ownable.sol";
 
-contract NFTMarketplace is ERC721 {
-    mapping(address => uint256) public tokenIdToPrice;
+contract NFTMarketplace is ERC721, Ownable {
+    using SafeMath for uint256;
 
-    event OfferCreated(address indexed owner, uint256 tokenId, uint256 price);
-    event OfferAccepted(address indexed buyer, uint256 tokenId, uint256 price);
-    event OfferCancelled(address indexed owner, uint256 tokenId, uint256 price);
+    mapping(uint256 => address) public tokenOwner;
+    mapping(uint256 => uint256) public tokenIdToPrice;
 
-    function createOffer(uint256 tokenId, uint256 price) public onlyOwnerOf(tokenId) {
-        require(tokenIdToPrice[tokenId] == 0, "Offer already exists for this token");
+    event NewTokenListing(
+        uint256 indexed tokenId,
+        uint256 price
+    );
+
+    event TokenSold(
+        uint256 indexed tokenId,
+        address indexed buyer,
+        uint256 price
+    );
+
+    constructor() ERC721("NFTMarketplace", "NFTM") public {}
+
+    function listTokenForSale(uint256 tokenId, uint256 price) public onlyOwner(tokenId) {
+        require(price > 0, "Price must be greater than zero.");
+        require(tokenIdToPrice[tokenId] == 0, "Token is already listed for sale.");
+
         tokenIdToPrice[tokenId] = price;
-        emit OfferCreated(msg.sender, tokenId, price);
+        emit NewTokenListing(tokenId, price);
     }
 
-    function acceptOffer(uint256 tokenId) public payable {
-        require(tokenIdToPrice[tokenId] != 0, "Offer does not exist for this token");
-        require(msg.value >= tokenIdToPrice[tokenId], "Offer price not met");
-        address owner = ownerOf(tokenId);
-        require(owner != address(0), "Token ownership not found");
-        tokenIdToPrice[tokenId] = 0;
-        emit OfferAccepted(msg.sender, tokenId, tokenIdToPrice[tokenId]);
-        owner.transfer(tokenIdToPrice[tokenId]);
-    }
+    function buyToken(uint256 tokenId) public payable {
+        require(tokenIdToPrice[tokenId] > 0, "Token is not for sale.");
+        require(msg.value >= tokenIdToPrice[tokenId], "Offered price is lower than the asking price.");
 
-    function cancelOffer(uint256 tokenId) public onlyOwnerOf(tokenId) {
-        require(tokenIdToPrice[tokenId] != 0, "Offer does not exist for this token");
+        address seller = tokenOwner[tokenId];
         uint256 price = tokenIdToPrice[tokenId];
+
+        _transfer(seller, msg.sender, tokenId);
+
+        seller.transfer(price);
         tokenIdToPrice[tokenId] = 0;
-        emit OfferCancelled(msg.sender, tokenId, price);
+
+        emit TokenSold(tokenId, msg.sender, price);
+    }
+
+    function safeTransferFrom(address from, address to, uint256 tokenId) public override {
+        require(isApprovedOrOwner(from, msg.sender), "Not authorized to transfer.");
+        require(from != address(0), "From address is zero.");
+        require(to != address(0), "To address is zero.");
+
+        _transfer(from, to, tokenId);
+    }
+
+    function isApprovedOrOwner(address spender, address owner) internal view returns (bool) {
+        return spender == owner || getApproved(tokenId) == spender;
+    }
+
+    function _transfer(address from, address to, uint256 tokenId) internal {
+        require(tokenOwner[tokenId] == from, "From address is not the owner.");
+        require(to != address(0), "To address is zero.");
+
+        tokenOwner[tokenId] = to;
+
+        emit Transfer(from, to, tokenId);
     }
 }
 
-//This code implements a simple NFT marketplace that allows owners to create offers for their NFTs, buyers to accept offers, and owners to cancel offers. The contract uses the ERC721 contract from OpenZeppelin as a base, which implements the ERC721 standard. The createOffer function allows an NFT owner to create an offer for their NFT at a certain price. The acceptOffer function allows a buyer to accept an offer by sending the required funds to the contract. The cancelOffer function allows an NFT owner to cancel an offer. The contract emits events for each offer creation, acceptance, and cancellation. This code is a sample and may not cover all the potential security considerations for an NFT marketplace contract.
+
+//This contract implements the ERC721 standard and also includes additional functionality for listing tokens for sale, buying tokens, and securely transferring tokens. The contract also
+
+
+
